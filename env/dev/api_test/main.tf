bebc1b444110b3cebec84e97f3809b30649a9bd4
NishimuraTakuya-nt <takuya.nishimura.chel@gmail.com>
2024-05-10 21:13:07 +0900

    add ec2

--- env/dev/api_test/main.tf
+++ env/dev/api_test/main.tf
@@ -12,7 +12,7 @@ data "terraform_remote_state" "vpc" {
 module "ec2_security_group_public" {
   source = "../../../modules/network/security_group"
 
-  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
+  vpc_id              = data.terraform_remote_state.vpc.outputs.vpc_id
   security_group_name = "dev-sg-public-01"
 
   ingress_rules = [
@@ -28,7 +28,7 @@ module "ec2_security_group_public" {
 module "ec2_security_group_private" {
   source = "../../../modules/network/security_group"
 
-  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
+  vpc_id              = data.terraform_remote_state.vpc.outputs.vpc_id
   security_group_name = "dev-sg-private-01"
 
   ingress_rules = [
@@ -39,4 +39,26 @@ module "ec2_security_group_private" {
       cidr_blocks = ["0.0.0.0/0"]
     }
   ]
+}
+
+# EC2
+module "ec2_public" {
+  source = "../../../modules/compute/ec2"
+
+  instance_name               = "public-ec2-test-01"
+  instance_type               = "t2.micro"
+  ami_id                      = "ami-0ab3794db9457b60a"
+  subnet_id                   = data.terraform_remote_state.vpc.outputs.public_subnet_id_1
+  vpc_security_group_ids      = [module.ec2_security_group_public.security_group_id]
+  associate_public_ip_address = true
+}
+
+module "ec2_private" {
+  source = "../../../modules/compute/ec2"
+
+  instance_name          = "private-ec2-test-01"
+  instance_type          = "t2.micro"
+  ami_id                 = "ami-0ab3794db9457b60a"
+  subnet_id              = data.terraform_remote_state.vpc.outputs.private_subnet_id_1
+  vpc_security_group_ids = [module.ec2_security_group_private.security_group_id]
 }
--- env/dev/vpc/main.tf
+++ env/dev/vpc/main.tf
@@ -47,7 +47,7 @@ module "private_subnet_2" {
 module "igw" {
   source = "../../../modules/network/internet_gateway"
 
-  vpc_id = module.vpc.vpc_id
+  vpc_id   = module.vpc.vpc_id
   igw_name = "n-plant-dev-igw"
 }
 
@@ -55,7 +55,7 @@ module "igw" {
 module "nat_gateway" {
   source = "../../../modules/network/nat_gateway"
 
-  subnet_id   = module.public_subnet_1.subnet_id
+  subnet_id        = module.public_subnet_1.subnet_id
   nat_gateway_name = "n-plant-dev-ngw"
 }
 
@@ -63,17 +63,17 @@ module "nat_gateway" {
 module "public_route_table" {
   source = "../../../modules/network/route_table"
 
-  vpc_id         = module.vpc.vpc_id
+  vpc_id           = module.vpc.vpc_id
   route_table_name = "n-plant-dev-public-1a-rt"
-  igw_id         = module.igw.igw_id
-  subnet_id = module.public_subnet_1.subnet_id
+  igw_id           = module.igw.igw_id
+  subnet_id        = module.public_subnet_1.subnet_id
 }
 
 module "private_route_table" {
   source = "../../../modules/network/route_table"
 
-  vpc_id         = module.vpc.vpc_id
+  vpc_id           = module.vpc.vpc_id
   route_table_name = "n-plant-dev-private-1a-rt"
-  nat_gateway_id = module.nat_gateway.nat_gateway_id
-  subnet_id = module.private_subnet_1.subnet_id
+  nat_gateway_id   = module.nat_gateway.nat_gateway_id
+  subnet_id        = module.private_subnet_1.subnet_id
 }
