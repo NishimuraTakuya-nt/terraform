fa108a0281a958ca62361170de4367cf35032748
NishimuraTakuya-nt <takuya.nishimura.chel@gmail.com>
2024-06-22 23:20:30 +0900

    add ecr

--- env/common/iam/main.tf
+++ env/common/iam/main.tf
@@ -105,6 +105,18 @@ resource "aws_iam_policy" "ci" {
             "cloudfront:DeleteOriginAccessControl",
             "cloudfront:ListOriginAccessControls",
             "cloudfront:ListTagsForResource",
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:BatchGetImage",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:PutImage",
+            "ecr:InitiateLayerUpload",
+            "ecr:UploadLayerPart",
+            "ecr:CompleteLayerUpload",
+            "ecr:DescribeRepositories",
+            "ecr:ListTagsForResource",
+            "ecr:TagResource",
+            "ecr:PutLifecyclePolicy",
+            "ecr:GetLifecyclePolicy",
           ]
           Effect   = "Allow"
           Resource = "*"
